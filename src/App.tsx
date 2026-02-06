df7dc60da9c8a09c987cda30d83ab3418c7d4b6e
eduardomzm <eduardo.mezam@outlook.com>
2026-02-06 16:04:40 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Header, Hero } from './components'
+
+function App() {
+
+  return (
+    <main className='w-[375px] m-auto'>
+
+      <section className='bg-Navy-850 p-6 h-[420px] relative'>
+      <Header />
+      <img 
+      src="/images/illustration-intro.png" 
+      alt="Intro illustration"
+      className='relative z-10 mt-12'
+      />
+      <img 
+      className='absolute bottom-0 left-0'
+      src="/images/bg-curvy-mobile.svg"
+      alt="bg curvy mobile"
+      />
+    
+      </section>
+
+      <section className='bg-Navy-900 p-6'>
+        <Hero />
+      </section>
+    </main>
+  )
+}
+
+export default App
